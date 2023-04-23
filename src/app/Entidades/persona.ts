dd28626fb125b64063c2dbf8b366070538512037
alejandro <alejocarreteroballesteros@gmail.com>
2023-04-23 15:44:43 -0500

    solicitud fetch

---
+++ src/app/Entidades/persona.ts
@@ -0,0 +1,17 @@
+export class persona{
+ 
+    
+    nombre : string;
+    edad :number;
+    cedula :number;
+    url:string
+    constructor(){
+        this.cedula=0;
+        this.edad=0;
+        this.nombre="";
+        this.url="";
+    }
+
+
+
+}
--- src/app/titulos/titulos.component.ts
+++ src/app/titulos/titulos.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from '../Entidades/persona';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-titulos',
   templateUrl: './titulos.component.html',
   styleUrls: ['./titulos.component.css']
 })
-export class TitulosComponent {
+export class TitulosComponent implements OnInit{
+  url="https://localhost:7115/weatherForecast";
 
+  valor:string="";
+   _persona = new persona();
+  ngOnInit(): void{
+  }
+  change_name(): void{
+    this._persona.cedula=3;
+    this._persona.edad=4;
+    this._persona.nombre="Soy daniel torres"
+    this._persona.url="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg"
+
+  }
+  btnsendclick(name:string):void{
+    console.log(name);
+    this.valor=name
+    this._persona.nombre=name;
+    fetch(this.url)
+    .then((response) => response.json())
+    .then(console.log);  
+  }
 }
